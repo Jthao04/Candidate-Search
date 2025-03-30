29723c11f898fb93aa44e4b17fe33a93dcf95106
Jethro <Jthao04@gmail.com>
2025-03-30 15:30:10 -0500

    new

--- Develop/src/api/API.tsx
+++ Develop/src/api/API.tsx
@@ -1,24 +1,29 @@
 const searchGithub = async () => {
   try {
-    const start = Math.floor(Math.random() * 100000000) + 1;
-    // console.log(import.meta.env);
+    console.log('GitHub Token:', import.meta.env.VITE_GITHUB_TOKEN);
+
+    const headers = {
+      Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+    };
+    console.log('Request Headers:', headers);
+
     const response = await fetch(
-      `https://api.github.com/users?since=${start}`,
-      {
-        headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
-        },
-      }
+      `https://api.github.com/users?since=1`,
+      { headers }
     );
-    // console.log('Response:', response);
+
+    console.log('Response:', response);
     const data = await response.json();
+
     if (!response.ok) {
-      throw new Error('invalid API response, check the network tab');
+      console.error('Error Response:', response);
+      throw new Error(`Error ${response.status}: ${data.message}`);
     }
-    // console.log('Data:', data);
+
+    console.log('GitHub users fetched successfully:', data);
     return data;
   } catch (err) {
-    // console.log('an error occurred', err);
+    console.error('Error fetching GitHub users:', err);
     return [];
   }
 };
